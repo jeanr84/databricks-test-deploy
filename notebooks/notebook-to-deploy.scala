40b65c8f16fdfbb8d7ba227adfdad9c8d1e7bf2e
jeanr84 <robinjeanr@gmail.com>
2020-01-29 18:00:34 +0100

    blfdmdfjb

--- notebooks/notebook-to-deploy.scala
+++ notebooks/notebook-to-deploy.scala
@@ -31,5 +31,5 @@ spark.conf.set("fs.adl.oauth2.refresh.url", dbutils.secrets.get(scope = "azure",
 
 // COMMAND ----------
 
-println("Ce print a été ajouté dans la version v2.0.0")
+println("Ce print a été ajouté dans la version bfdkfdk v2.0.0")
 spark.read.parquet(adlsPrefix + atomePath).count()
